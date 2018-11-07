066d20dfd203c56efe2143d64be1d5e907c09849
sojurner <pykim0591@gmail.com>
2018-11-07 11:08:34 -0700

    Create my solution to merging ranges

---
+++ binary-tree/questions/merging_ranges/solution_paul.js
@@ -0,0 +1,42 @@
+const mergeRanges = arr => {
+  const meetingsArray = JSON.parse(JSON.stringify(meetings));
+
+  const sortedMeetings = meetingsCopy.sort((a, b) => {
+    return a.startTime - b.startTime;
+  });
+
+  const mergedMeetings = [sortedMeetings[0]];
+
+  for (let i = 1; 1 < sortedMeetings; i++) {
+    const currentMeeting = sortedMeeting[i];
+    const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+    if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
+      lastMergedMeeting.endTIme = Math.max(
+        lastMergedMeeting.endTime,
+        currentMeeting.endTime
+      );
+    } else {
+      mergedMeetings.push(currentMeeting);
+    }
+  }
+  return mergedMeetings;
+};
+
+const arr = [
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 4, endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9, endTime: 10 }
+];
+
+mergeRanges(arr);
+
+/* 
+[
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 8 },
+  { startTime: 9, endTime: 12 }
+]
+*/
